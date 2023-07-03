03607b2f82958550309d9f2f128d19e58cdc9ddb
Juan Alvarez <alvarez.juanign@gmail.com>
2023-07-03 20:41:00 -0300

    Edit description of project

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export function App () {
       <Skills />
       <Project
         projectName='CABAÑAS LA ARBOLEDA'
-        descriptionProject='Landing page de unas cabañas ubicadas en San luis. Tp final de un curso dado por la UTN.'
+        descriptionProject='Landing page de unas cabañas ubicadas en San luis. Proyecto final- BA multiplica 2.0.'
       />
       <footer className='bg-[#3463fd] text-white w-full'>
         <p className='text-center'>©️ Todos los derechos reservados.</p>
