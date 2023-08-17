f3578c97417797ff4e65a1402b66fe7a43b1ca24
Juan Alvarez <alvarez.juanign@gmail.com>
2023-08-17 18:24:55 -0300

    refactor render of projects

--- src/logic/MappedProjects.jsx
+++ src/logic/MappedProjects.jsx
@@ -1,42 +1,42 @@
-import GITHUB from '../assets/github-svgrepo-com.svg'
-import DEPLOY from '../assets/web-svgrepo-com.svg'
+import logoGithub from '../assets/github-svgrepo-com.svg'
+import logoDeploy from '../assets/web-svgrepo-com.svg'
 
 export function MappedProjects ({ projects }) {
   return (
-    <section>
-      <h3 className='text-center font-bold text-2xl underline dark:text-white'>Projects</h3>
-      <div className='flex flex-wrap justify-center'>
-        {
-        projects.map(project => (
-          <article className='p-5 flex flex-wrap justify-center' key={project.id}>
-            <div className='rounded-md max-h-[500px] max-w-[400px] p-2 flex flex-col items-center justify-evenly gap-5 border border-black shadow-xl dark:border-white dark:bg-gray-200'>
-              <h4 className='font-bold cursor-default'>{project.title}</h4>
-              <img className='border border-black m-1' src={project.image} />
-              <p className='m-2 cursor-default font-medium'>{project.description}</p>
-              <div className='flex w-full justify-around gap-2'>
+    <section className='max-w-4xl my-9 px-5'>
+      <h3 className='text-center mb-3 font-bold text-2xl underline dark:text-white'>
+        Projects
+      </h3>
+      <article className='flex flex-row flex-wrap justify-center gap-4 w-full'>
+        {projects.map((project) => (
+          <div key={project.id} className='relative max-w-[400px]'>
+            <img src={project.image} className='rounded-lg' />
+            <div
+              className='absolute top-0 bg-black/90 rounded-lg text-white h-full w-full p-3 opacity-0 hover:opacity-100 flex flex-col justify-center gap-4 transition-opacity duration-500 cursor-pointer'
+            >
+              <h3 className='text-lg font-bold'>{project.title}</h3>
+              <p>{project.description}</p>
+              <section
+                className='flex w-full justify-around p-3'
+              >
                 <div className='flex gap-2'>
                   <a href={project.linkGithub}>
-                    <img src={GITHUB} className='h-8' />
+                    <img src={logoGithub} className='h-8' />
                   </a>
                   <a href={project.linkDeploy}>
-                    <img src={DEPLOY} className='h-8 scale-95' />
+                    <img src={logoDeploy} className='h-8 scale-95 bg-white rounded-full' />
                   </a>
                 </div>
                 <div className='flex'>
-                  {project.techs.map(tech => (
-                    <img
-                      key={tech}
-                      src={tech}
-                      className='h-7'
-                    />
+                  {project.techs.map((tech) => (
+                    <img key={tech} src={tech} className='h-7' />
                   ))}
                 </div>
-              </div>
+              </section>
             </div>
-          </article>
-        ))
-        }
-      </div>
+          </div>
+        ))}
+      </article>
     </section>
   )
 }
