6cbeaf8393f6774abd9e43194c7c8a90f366ec72
Juan Alvarez <alvarez.juanign@gmail.com>
2023-08-18 19:20:51 -0300

    Styling projects hover

--- src/logic/MappedProjects.jsx
+++ src/logic/MappedProjects.jsx
@@ -10,30 +10,41 @@ export function MappedProjects ({ projects }) {
       <article className='flex flex-row flex-wrap justify-center gap-4 w-full'>
         {projects.map((project) => (
           <div key={project.id} className='relative max-w-[400px]'>
-            <img src={project.image} alt='image of the website' className='rounded-lg' />
-            <div
-              className='absolute top-0 bg-black/90 rounded-lg text-white h-full w-full p-3 opacity-0 hover:opacity-100 flex flex-col justify-center gap-4 transition-opacity duration-500 cursor-pointer'
-            >
-              <h3 className='text-lg font-bold'>{project.title}</h3>
-              <p>{project.description}</p>
-              <section
-                className='flex w-full justify-around p-3'
-              >
-                <div className='flex gap-2'>
-                  <a href={project.linkGithub}>
-                    <img src={logoGithub} alt='github logo' className='h-8' />
-                  </a>
-                  <a href={project.linkDeploy}>
-                    <img src={logoDeploy} alt='deploy logo' className='h-8 scale-95 bg-white rounded-full' />
-                  </a>
-                </div>
-                <div className='flex'>
-                  {project.techs.map((tech) => (
-                    <img key={tech} alt='logo of techs' src={tech} className='h-7' />
-                  ))}
-                </div>
-              </section>
-            </div>
+            <img
+              src={project.image}
+              alt='image of the website'
+              className='rounded-lg'
+            />
+            <article className='absolute top-0 bg-black/90 rounded-lg text-white h-full w-full p-3 opacity-0 hover:opacity-100 flex items-center transition-opacity duration-500 cursor-pointer'>
+              <div className='max-h-36 flex flex-col justify-center gap-2'>
+                <h3 className='text-xl font-bold'>{project.title}</h3>
+                <p className='text-lg'>{project.description}</p>
+                <section className='flex w-full justify-around p-3'>
+                  <div className='flex gap-2'>
+                    <a href={project.linkGithub}>
+                      <img src={logoGithub} alt='github logo' className='h-8' />
+                    </a>
+                    <a href={project.linkDeploy}>
+                      <img
+                        src={logoDeploy}
+                        alt='deploy logo'
+                        className='h-8 scale-95 bg-white rounded-full'
+                      />
+                    </a>
+                  </div>
+                  <div className='flex'>
+                    {project.techs.map((tech) => (
+                      <img
+                        key={tech}
+                        alt='logo of techs'
+                        src={tech}
+                        className='h-7'
+                      />
+                    ))}
+                  </div>
+                </section>
+              </div>
+            </article>
           </div>
         ))}
       </article>
