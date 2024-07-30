b91ec9c5ee3bd0f5480176a9fd623024f6cd1a42
Juan Alvarez <alvarez.juanign@gmail.com>
2024-07-30 15:09:54 -0300

    change heart

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { About } from './components/About'
 import { Projects } from './components/Projects'
 import { Skills } from './components/Skills'
 
-export function App () {
+export function App() {
   return (
     <main className='flex flex-col items-center'>
       <About />
       <Projects />
       <Skills />
       <footer className='bg-[#3463fd] text-white w-full'>
-        <p className='text-center p-4'>Made by Alvarez Juan 🤍</p>
+        <p className='text-center p-4'>Made by Alvarez Juan ❤️</p>
       </footer>
     </main>
   )
